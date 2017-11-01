304c83aeb9426a1fb1e96550653923f5ca975449
Pradyuman Vig <pradyumanvig@outlook.com>
2017-11-01 12:35:36 -0400

    hornStrobe boolean

--- server/state.go
+++ server/state.go
@@ -1,27 +1,27 @@
 package server
 
-type Status struct{
-	LeftWindowUp bool `json:"leftWindowUp"`
-	LeftWindowDown bool `json:"leftWindowDown"`
-	RightWindowUp bool `json:"rightWindowUp"`
-	RightWindowDown bool `json:"rightWindowDown"`
-	LeftDoorLocked bool `json:"leftDoorLocked"`
-	RightDoorLocket bool `json:"rightDoorLocked"`
-	TrunkOpen bool `json:"trunkOpen"`
-	HazardOn bool `json:"hazardOn"`
-	PopupOn bool `json:"popupOn"`
-	HornOn bool `json:"hornOn"`
-	HornMode string `json:"hornMode"`
-	McACOn bool `json:"mcACOn"`
-	CarACOn bool `json:"carACOn"`
-	McFanOn bool `json:"mcFanOn"`
-	CarFanOn bool `json:"carFanOn"`
-	McAccessoryOn bool `json:"mcAccessoryOn"`
-	CarAccessoryOn bool `json:"carAccessoryOn"`
-	McIG1On bool `json:"mcIG1On"`
-	CarIG1On bool `json:"carIG1On"`
-	McIG2On bool `json:"mcIG2On"`
-	CarIG2On bool `json:"carIG2On"`
-	EngineRunning bool `json:"engineRunning"`
+type Status struct {
+	LeftWindowUp       bool `json:"leftWindowUp"`
+	LeftWindowDown     bool `json:"leftWindowDown"`
+	RightWindowUp      bool `json:"rightWindowUp"`
+	RightWindowDown    bool `json:"rightWindowDown"`
+	LeftDoorLocked     bool `json:"leftDoorLocked"`
+	RightDoorLocket    bool `json:"rightDoorLocked"`
+	TrunkOpen          bool `json:"trunkOpen"`
+	HazardOn           bool `json:"hazardOn"`
+	PopupOn            bool `json:"popupOn"`
+	HornOn             bool `json:"hornOn"`
+	HornStrobe         bool `json:"hornStrobe"`
+	McACOn             bool `json:"mcACOn"`
+	CarACOn            bool `json:"carACOn"`
+	McFanOn            bool `json:"mcFanOn"`
+	CarFanOn           bool `json:"carFanOn"`
+	McAccessoryOn      bool `json:"mcAccessoryOn"`
+	CarAccessoryOn     bool `json:"carAccessoryOn"`
+	McIG1On            bool `json:"mcIG1On"`
+	CarIG1On           bool `json:"carIG1On"`
+	McIG2On            bool `json:"mcIG2On"`
+	CarIG2On           bool `json:"carIG2On"`
+	EngineRunning      bool `json:"engineRunning"`
 	BluetoothConnected bool `json:"bluetoothConnected"`
 }
